1b3da1927c219ab493f73bf07f0a52e4cf8de060
Professor Dev <isma.salgeriev@bk.ru>
2026-01-14 00:25:36 +0300

    обновил иконку и анимацию меню-бургер

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -108,7 +108,6 @@ export default function Navigation() {
                 <span className={styles.burgerIcon} aria-hidden="true">
                   <span className={styles.burgerLine} />
                   <span className={styles.burgerLine} />
-                  <span className={styles.burgerLine} />
                 </span>
                 <span className={styles.srOnly}>Меню</span>
               </button>
