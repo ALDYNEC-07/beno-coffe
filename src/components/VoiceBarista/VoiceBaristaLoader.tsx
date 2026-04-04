3e64c39070784a785d90889eb7f433dd99d531d6
Professor Dev <isma.salgeriev@bk.ru>
2026-04-05 01:20:27 +0300

    Revert "Merge pull request #33 from ALDYNEC-07/AI-Barist"
    This reverts commit b40f640eaa336183863c8f037f12c798116b09f2, reversing
    changes made to 8b1e9e2a61b5e2b9f1163b52e7e940b12d21a7da.

--- src/components/VoiceBarista/VoiceBaristaLoader.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const VoiceBarista = dynamic(() => import("./VoiceBarista"), { ssr: false });
-
-export default function VoiceBaristaLoader() {
-  return <VoiceBarista />;
-}
